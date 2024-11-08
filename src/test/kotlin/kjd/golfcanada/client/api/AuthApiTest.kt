62f0bfd34fc7fa2d4dc08ad9c1c5e74be7bbc2d5
Ken Davidson <ken.j.davidson@live.ca>
2024-11-07 23:27:22 -0500

    chore(token): update api grant_type options

--- src/test/kotlin/kjd/golfcanada/client/api/AuthApiTest.kt
+++ src/test/kotlin/kjd/golfcanada/client/api/AuthApiTest.kt
@@ -5,6 +5,7 @@ import io.kotest.core.spec.style.DescribeSpec
 import io.kotest.matchers.shouldBe
 import io.kotest.matchers.shouldNotBe
 import kjd.golfcanada.auth.AuthenticationHandler.Companion.DEFAULT_SCOPES
+import kjd.golfcanada.client.api.AuthApi.GrantTypeGetAuthToken
 
 @EnabledIf(UsernamePasswordCondition::class)
 class AuthApiTest: DescribeSpec({
@@ -20,14 +21,23 @@ class AuthApiTest: DescribeSpec({
 
     context("getAuthToken") {
         it("should login successfully") {
-            val authToken = authApi.getAuthToken("password", username, password, DEFAULT_SCOPES)
+            val authToken = authApi.getAuthToken(GrantTypeGetAuthToken.PASSWORD, DEFAULT_SCOPES, username = username, password = password)
 
-            print(authToken)
+            println(authToken)
 
             authToken shouldNotBe null
             authToken.accessToken shouldNotBe null
             authToken.refreshToken shouldNotBe null
             authToken.expiresIn shouldBe 3600
+
+            val refreshedToken = authApi.getAuthToken(GrantTypeGetAuthToken.REFRESH_TOKEN, DEFAULT_SCOPES, refreshToken = authToken.refreshToken)
+
+            println(refreshedToken)
+
+            refreshedToken shouldNotBe null
+            refreshedToken.accessToken shouldNotBe null
+            refreshedToken.refreshToken shouldNotBe null
+            refreshedToken.expiresIn shouldBe 3600
         }
     }
 })
